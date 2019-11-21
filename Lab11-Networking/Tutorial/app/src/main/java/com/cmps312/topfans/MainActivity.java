9f53ef2b6e47b69b8488c332a0f43ba31584e06f
abdalabaaji <abdala.baaji@gmail.com>
2019-11-21 16:52:21 +0300

    update 4:52

--- Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/topfans/MainActivity.java
+++ Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/topfans/MainActivity.java
@@ -1,25 +1,29 @@
 package com.cmps312.topfans;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.cmps312.topfans.httpRequests.UsersClient;
 import com.cmps312.topfans.models.Result;
+import com.cmps312.topfans.models.User;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.Retrofit.Builder;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.cmps312.topfans.httpRequests.UsersClient.BASE_URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MYMainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,11 +51,17 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<Result>() {
             @Override
             public void onResponse(Call<Result> call, Response<Result> response) {
-                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                Result result = response.body();
+                ArrayList<User> users = result.getUsers();
+
+                User user = users.get(0);
+
+                Toast.makeText(MainActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFailure(Call<Result> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage() + "\n" + t.getCause());
                 Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
 
             }
--- Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/topfans/models/User.java
+++ Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/topfans/models/User.java
@@ -3,6 +3,24 @@ package com.cmps312.topfans.models;
 public class User {
     private String gender;
     private String email;
+    private Name name;
+    private Picture picture;
+
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Picture picture) {
+        this.picture = picture;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
 
     public String getGender() {
         return gender;
@@ -18,5 +36,46 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public class Name{
+        private String title;
+        private String first;
+        private String last;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getFirst() {
+            return first;
+        }
+
+        public void setFirst(String first) {
+            this.first = first;
+        }
+
+        public String getLast() {
+            return last;
+        }
+
+        public void setLast(String last) {
+            this.last = last;
+        }
+    }
+    public class Picture{
+        private String large;
+
+        public String getLarge() {
+            return large;
+        }
+
+        public void setLarge(String large) {
+            this.large = large;
+        }
     }
 }
